5fb50415bcf4447ee6b3fc0f184cd5217201dc10
pankaj verma <60501879+pv05@users.noreply.github.com>
2022-02-22 05:37:36 +0530

    after movie recommand project added

---
+++ sms-spam-collection-dataset/app.py
@@ -0,0 +1,64 @@
+import streamlit as st 
+import pickle
+import nltk
+from nltk.corpus import stopwords
+import string
+from nltk.stem.porter import PorterStemmer
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+def transform_text(text):
+    #Lower case
+    text = text.lower()
+    
+    #Tokenization
+    text = nltk.word_tokenize(text) 
+    
+    #Removing special characters
+    y = []
+    for i in text:
+        if i.isalnum(): 
+            y.append(i)
+    
+    #Removing stop words and punctuation
+    text = y[:] 
+    y.clear()
+    
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    
+    #Stemming (convert similar words to a specif word like dancing,dance,danced conver to danc)
+    text = y[:] 
+    y.clear()   
+     
+    ps = PorterStemmer()
+    for i in text:
+        y.append(ps.stem(i))
+    
+
+    return " ".join(y)
+
+
+
+st.title('SMS Spam Classifer')
+
+input_sms = st.text_area('Enter the SMS')
+
+
+if st.button('predict'):
+    if len(input_sms.strip()) != 0: 
+        # preprocessing
+        transformed_sms = transform_text(input_sms)
+        # vectorizer
+        vector_input = tfidf.transform([transformed_sms])
+        # predict
+        result = model.predict(vector_input)[0]
+        # display
+        if result == 1:
+            st.header('Spam')
+        else:
+            st.header('Not Spam')
+    else:
+        st.header('TextArea is Blank')
