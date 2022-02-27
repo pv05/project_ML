3d16bef8c9293951cdf72cc28cfda0b49293afa8
pankaj verma <60501879+pv05@users.noreply.github.com>
2022-02-27 12:57:09 +0530

    app.py some changes in pkl file

--- sms-spam-collection-dataset/app.py
+++ sms-spam-collection-dataset/app.py
@@ -5,8 +5,14 @@ from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+#tfidf = pickle.load(open('vectorizer.pkl','rb'))
+#model = pickle.load(open('model.pkl','rb'))
+
+path =https://github.com/pv05/project_ML/blob/main/sms-spam-collection-dataset
+tfidf = pickle.load(open(r'{path}/vectorizer.pkl','rb'))
+model = pickle.load(open(r'{path}/model.pkl','rb'))
+
+
 
 def transform_text(text):
     #Lower case
