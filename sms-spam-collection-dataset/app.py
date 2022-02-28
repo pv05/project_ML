3515e32d82781b7cc4e8387cfd7d9e2ddca8a213
pankaj verma <60501879+pv05@users.noreply.github.com>
2022-02-28 21:37:20 +0530

    app.py some changes in pkl file

--- sms-spam-collection-dataset/app.py
+++ sms-spam-collection-dataset/app.py
@@ -10,8 +10,8 @@ from nltk.stem.porter import PorterStemmer
 # model = pickle.load(urllib.request.urlopen('https:///raw.githubusercontent.com/pv05/project_ML/tree/main/sms-spam-collection-dataset/model.pkl'))
 
 
-tfidf = pickle.load(open('/pv05/project_ML/blob/main/sms-spam-collection-dataset/vectorizer.pkl','rb'))
-model = pickle.load(open('/pv05/project_ML/blob/main/sms-spam-collection-dataset/model.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 
 
